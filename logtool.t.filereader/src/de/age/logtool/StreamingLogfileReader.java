34bb539961bc40558abffcceb75fdcd3e690187b
Alexander Gerhardt <h.a.gerhardt@googlemail.com>
2011-12-05 11:09:33 +0100

    changed the reader to the task itself, so i can test step by step

--- logtool.t.filereader/src/de/age/logtool/StreamingLogfileReader.java
+++ logtool.t.filereader/src/de/age/logtool/StreamingLogfileReader.java
@@ -1,69 +1,77 @@
 package de.age.logtool;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Timer;
 import java.util.TimerTask;
 
-import de.age.logtool.exceptions.IllegalLogfileListenerException;
+import de.age.logtool.util.TrackingLogfileListener;
 
-/**
- * Watches a logfile and sends out events with new content when it becomes available.
- */
-public class StreamingLogfileReader {
-
-	private final Reader reader;
-	private final List<LogfileListener> listeners;
+public class StreamingLogfileReader extends TimerTask {
+	private static final int BUFFER_SIZE = 1024;
 	
+	private final StringBuilder savedResult = new StringBuilder();
+	private final char[] buffer;
+	private long position;
+	private long lastModification;
+	private final List<LogfileListener> listeners;
+	private final File file;
+
 	public StreamingLogfileReader(File file) {
+		this.file = file;
+		buffer = new char[BUFFER_SIZE];
+		lastModification = -1;
+		position = 0;
 		listeners = new ArrayList<LogfileListener>();
+	}
+
+	@Override
+	public void run() {
+		long modificationTime = file.lastModified();
+		if (file.exists() && modificationTime > lastModification) {
+			lastModification = modificationTime;
+			readNewDataFromFile();
+		}
+	}
+
+	private void readNewDataFromFile() {
+		Reader reader = null;
 		try {
 			reader = new FileReader(file);
-		} catch (FileNotFoundException e) {
-			throw new RuntimeException();
-		}
-		Timer timer = new Timer(true);
-		timer.schedule(new FileSurveillanceTask(), 0, 200);
-	}
-	
-	public void addLogfileListener(LogfileListener listener) {
-		if (listener == null) {
-			throw IllegalLogfileListenerException.nullListener();
-		}
-		listeners.add(listener);
-	}
-	
-	private final class FileSurveillanceTask extends TimerTask {
-		private final StringBuilder buffer = new StringBuilder();
-		
-		@Override
-		public void run() {
-			char[] characters = new char[1024];
-			try {
-				int read = reader.read(characters);
-				if (read > 0) {
-					buffer.append(characters, 0, read);
+			reader.skip(position);
+			int read = reader.read(buffer);
+			while (read >= 0) {
+				position = position + read;
+				savedResult.append(buffer, 0, read);
+				read = reader.read(buffer);
+			}
+			int endOfLine = savedResult.indexOf("\n");
+			while (endOfLine >= 0) {
+				fireEvent(savedResult.substring(0, endOfLine));
+				savedResult.delete(0, endOfLine + 1);
+				endOfLine = savedResult.indexOf("\n");
+			}
+		} catch (IOException e) {
+		} finally {
+			if (reader != null) {
+				try {
+					reader.close();
+				} catch (IOException e) {
 				}
-				int endOfLine = buffer.indexOf("\n");
-				if (endOfLine >= 0) {
-					fireEvent(buffer.substring(0, endOfLine));
-					buffer.delete(0, endOfLine + 1);
-				}
-			} catch (IOException e) {
-				throw new RuntimeException();
 			}
 		}
+	}
 
-		private void fireEvent(String substring) {
-			for (LogfileListener listener : listeners) {
-				listener.lineRead(substring);
-			}
+	private void fireEvent(String substring) {
+		for (LogfileListener listener : listeners) {
+			listener.lineRead(substring);
 		}
 	}
-		
+
+	public void addLogfileListener(TrackingLogfileListener listener) {
+		this.listeners.add(listener);
+	}
 }
