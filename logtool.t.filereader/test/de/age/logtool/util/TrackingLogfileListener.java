8a8bf0c4352341500145cd5bf5d36f1a27fcb173
Alexander Gerhardt <h.a.gerhardt@googlemail.com>
2011-12-05 17:33:07 +0100

    new helper method

--- logtool.t.filereader/test/de/age/logtool/util/TrackingLogfileListener.java
+++ logtool.t.filereader/test/de/age/logtool/util/TrackingLogfileListener.java
@@ -1,11 +1,15 @@
 package de.age.logtool.util;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+
+import org.hamcrest.Description;
+import org.hamcrest.SelfDescribing;
 
 import de.age.logtool.LogfileListener;
 
-public class TrackingLogfileListener implements LogfileListener {
+public class TrackingLogfileListener implements LogfileListener, SelfDescribing {
 
 	private final List<String> lines = new ArrayList<String>();
 	
@@ -16,6 +20,15 @@ public class TrackingLogfileListener implements LogfileListener {
 
 	public int getNumberOFLines() {
 		return lines.size();
+	}
+	
+	public List<String> getLines() {
+		return Collections.unmodifiableList(lines);
+	}
+	
+	@Override
+	public void describeTo(Description description) {
+		description.appendValueList("[", ",", "]", lines);
 	}
 
 }
