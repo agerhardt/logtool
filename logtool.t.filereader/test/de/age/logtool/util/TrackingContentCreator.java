d1ebf60d9eae98edb45653cd0544638191fd55d3
Alexander Gerhardt <h.a.gerhardt@googlemail.com>
2011-12-05 17:33:01 +0100

    new helper method fixed the content

--- logtool.t.filereader/test/de/age/logtool/util/TrackingContentCreator.java
+++ logtool.t.filereader/test/de/age/logtool/util/TrackingContentCreator.java
@@ -2,10 +2,14 @@ package de.age.logtool.util;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 
-public class TrackingContentCreator implements ContentCreator {
+import org.hamcrest.Description;
+import org.hamcrest.SelfDescribing;
+
+public class TrackingContentCreator implements ContentCreator, SelfDescribing {
 	
 	private static final SimpleDateFormat dateformat = new SimpleDateFormat("yyMMdd,HH:mm:ss.SSS");;
 	private final List<String> entries = new ArrayList<String>();
@@ -16,9 +20,23 @@ public class TrackingContentCreator implements ContentCreator {
 		newContent.append(dateformat.format(new Date()));
 		newContent.append(" :: Contententry [");
 		newContent.append(entries.size());
-		newContent.append("]\n");
+		newContent.append("]");
 		entries.add(newContent.toString());
+		newContent.append("\n");
 		return newContent.toString();
 	}
+	
+	public int getNumberOfEntries() {
+		return entries.size();
+	}
+	
+	public List<String> getEntries() {
+		return Collections.unmodifiableList(entries);
+	}
 
+	@Override
+	public void describeTo(Description description) {
+		description.appendValueList("[", ",", "]", entries);
+	}
+	
 }
