8f14659ffe4a49c8408d299ace023a3b00bbac7d
Alexander Gerhardt <h.a.gerhardt@googlemail.com>
2011-12-10 22:33:05 +0100

    added pattern

--- logtool.t.parsing/src/de/age/logtool/parsing/WebsphereLogParser.java
+++ logtool.t.parsing/src/de/age/logtool/parsing/WebsphereLogParser.java
@@ -3,14 +3,17 @@ package de.age.logtool.parsing;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.regex.Pattern;
 
 
 public class WebsphereLogParser extends AbstractLogParser {
 
 	private static final SimpleDateFormat FORMAT = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss:SSS");
+	private Pattern pattern;
 	private StringBuilder buffer;
 	
 	public WebsphereLogParser() {
+		pattern = Pattern.compile("\\[\\d[2]\\.\\d[2]\\.\\d[2] \\d[2]:\\d[2]:\\d[2]:\\d[3] CET\\].*");
 		buffer = new StringBuilder();
 	}
 	
@@ -31,12 +34,14 @@ public class WebsphereLogParser extends AbstractLogParser {
 	 * Parses the current content of the buffer as one event and fires that event.
 	 */
 	private void flushBuffer() {
+		if (pattern.matcher(buffer.toString()).find()) {
 		try {
 			Date parse = FORMAT.parse(buffer.substring(1, 24));
 			fireLogEvent(parse.getTime(), buffer.toString());
 		} catch (ParseException e) {
 			// TODO
 			throw new RuntimeException();
+		}
 		}
 		buffer = new StringBuilder();
 	}
