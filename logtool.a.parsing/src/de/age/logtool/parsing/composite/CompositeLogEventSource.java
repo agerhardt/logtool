d68da95086e930fbb49dafbabc810e70e2149f75
Alexander Gerhardt <h.a.gerhardt@googlemail.com>
2012-02-23 17:28:44 +0100

    using existing class or some basic functionality

--- logtool.a.parsing/src/de/age/logtool/parsing/composite/CompositeLogEventSource.java
+++ logtool.a.parsing/src/de/age/logtool/parsing/composite/CompositeLogEventSource.java
@@ -2,22 +2,27 @@ package de.age.logtool.parsing.composite;
 
 import de.age.logtool.parsing.LogEventListener;
 import de.age.logtool.parsing.LogEventSource;
+import de.age.logtool.parsing.util.LogListenerList;
 
 public class CompositeLogEventSource implements LogEventSource {
 
+	private final LogListenerList listeners;
+	
+	public CompositeLogEventSource() {
+		listeners = new LogListenerList();
+	}
+	
 	public void addLogEventSource(LogEventSource newSource) {
-		
+		// TODO Auto-generated method stub
 	}
 
 	@Override
 	public void addLogEventListener(LogEventListener l) {
-		// TODO Auto-generated method stub
-		
+		listeners.addLogEventListener(l);
 	}
 
 	@Override
 	public void removeLogEventListener(LogEventListener l) {
-		// TODO Auto-generated method stub
-		
+		listeners.removeLogEventListener(l);
 	}
 }
